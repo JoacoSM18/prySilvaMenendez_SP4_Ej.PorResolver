359e1aec7b8b3eb8595a6c23a5618cdbd5ae1268
JoacoSM18 <joako.sm.18@gmail.com>
2025-11-07 01:00:29 -0300

    Programa Listo

--- prySilvaMenendez_SP4_Ej.PorResolver/frmBarRegistroVentas.Designer.cs
+++ prySilvaMenendez_SP4_Ej.PorResolver/frmBarRegistroVentas.Designer.cs
@@ -36,15 +36,24 @@
             this.colBebidasSAlcohol = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colBebidasCAlcohol = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colPostres = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnMozodelDia = new System.Windows.Forms.Button();
+            this.btnTotales = new System.Windows.Forms.Button();
+            this.lblMozodelDia = new System.Windows.Forms.Label();
+            this.lblTotales = new System.Windows.Forms.Label();
+            this.lblImporteMozoDia = new System.Windows.Forms.Label();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).BeginInit();
             this.SuspendLayout();
             // 
             // btnValidarDatos
             // 
-            this.btnValidarDatos.Location = new System.Drawing.Point(744, 449);
-            this.btnValidarDatos.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnValidarDatos.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnValidarDatos.Location = new System.Drawing.Point(807, 27);
             this.btnValidarDatos.Name = "btnValidarDatos";
-            this.btnValidarDatos.Size = new System.Drawing.Size(140, 51);
+            this.btnValidarDatos.Size = new System.Drawing.Size(101, 36);
             this.btnValidarDatos.TabIndex = 1;
             this.btnValidarDatos.Text = "Validar Datos";
             this.btnValidarDatos.UseVisualStyleBackColor = true;
@@ -55,7 +64,7 @@
             this.dgvDatos.AllowUserToAddRows = false;
             this.dgvDatos.AllowUserToResizeColumns = false;
             this.dgvDatos.AllowUserToResizeRows = false;
-            this.dgvDatos.BackgroundColor = System.Drawing.Color.PaleGoldenrod;
+            this.dgvDatos.BackgroundColor = System.Drawing.Color.Goldenrod;
             this.dgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvDatos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.colMozos,
@@ -63,14 +72,14 @@
             this.colBebidasSAlcohol,
             this.colBebidasCAlcohol,
             this.colPostres});
-            this.dgvDatos.Location = new System.Drawing.Point(12, 41);
+            this.dgvDatos.Location = new System.Drawing.Point(8, 27);
+            this.dgvDatos.Margin = new System.Windows.Forms.Padding(2);
             this.dgvDatos.Name = "dgvDatos";
             this.dgvDatos.RowHeadersVisible = false;
             this.dgvDatos.RowHeadersWidth = 62;
             this.dgvDatos.RowTemplate.Height = 28;
-            this.dgvDatos.Size = new System.Drawing.Size(898, 362);
+            this.dgvDatos.Size = new System.Drawing.Size(753, 235);
             this.dgvDatos.TabIndex = 2;
-            this.dgvDatos.CellValidating += new System.Windows.Forms.DataGridViewCellValidatingEventHandler(this.dgvDatos_CellValidating);
             // 
             // colMozos
             // 
@@ -117,17 +126,120 @@
             this.colPostres.Resizable = System.Windows.Forms.DataGridViewTriState.False;
             this.colPostres.Width = 150;
             // 
+            // btnMozodelDia
+            // 
+            this.btnMozodelDia.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMozodelDia.Location = new System.Drawing.Point(153, 285);
+            this.btnMozodelDia.Margin = new System.Windows.Forms.Padding(2);
+            this.btnMozodelDia.Name = "btnMozodelDia";
+            this.btnMozodelDia.Size = new System.Drawing.Size(101, 45);
+            this.btnMozodelDia.TabIndex = 3;
+            this.btnMozodelDia.Text = "Mostrar Mozo del Dia";
+            this.btnMozodelDia.UseVisualStyleBackColor = true;
+            this.btnMozodelDia.Click += new System.EventHandler(this.btnMozodelDia_Click);
+            // 
+            // btnTotales
+            // 
+            this.btnTotales.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTotales.Location = new System.Drawing.Point(153, 334);
+            this.btnTotales.Margin = new System.Windows.Forms.Padding(2);
+            this.btnTotales.Name = "btnTotales";
+            this.btnTotales.Size = new System.Drawing.Size(101, 42);
+            this.btnTotales.TabIndex = 4;
+            this.btnTotales.Text = "Mostrar Totales";
+            this.btnTotales.UseVisualStyleBackColor = true;
+            this.btnTotales.Click += new System.EventHandler(this.btnTotales_Click);
+            // 
+            // lblMozodelDia
+            // 
+            this.lblMozodelDia.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMozodelDia.Location = new System.Drawing.Point(269, 294);
+            this.lblMozodelDia.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblMozodelDia.Name = "lblMozodelDia";
+            this.lblMozodelDia.Size = new System.Drawing.Size(101, 25);
+            this.lblMozodelDia.TabIndex = 5;
+            this.lblMozodelDia.Text = " ";
+            // 
+            // lblTotales
+            // 
+            this.lblTotales.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotales.Location = new System.Drawing.Point(269, 347);
+            this.lblTotales.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTotales.Name = "lblTotales";
+            this.lblTotales.Size = new System.Drawing.Size(101, 23);
+            this.lblTotales.TabIndex = 6;
+            this.lblTotales.Text = "  ";
+            this.lblTotales.Click += new System.EventHandler(this.lblTotales_Click);
+            // 
+            // lblImporteMozoDia
+            // 
+            this.lblImporteMozoDia.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblImporteMozoDia.Location = new System.Drawing.Point(374, 294);
+            this.lblImporteMozoDia.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblImporteMozoDia.Name = "lblImporteMozoDia";
+            this.lblImporteMozoDia.Size = new System.Drawing.Size(101, 25);
+            this.lblImporteMozoDia.TabIndex = 7;
+            this.lblImporteMozoDia.Text = " ";
+            // 
+            // lbl1
+            // 
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(374, 347);
+            this.lbl1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(101, 25);
+            this.lbl1.TabIndex = 8;
+            this.lbl1.Text = "  ";
+            // 
+            // lbl2
+            // 
+            this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.Location = new System.Drawing.Point(479, 347);
+            this.lbl2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(101, 25);
+            this.lbl2.TabIndex = 9;
+            this.lbl2.Text = "  ";
+            // 
+            // lbl3
+            // 
+            this.lbl3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3.Location = new System.Drawing.Point(584, 347);
+            this.lbl3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(101, 25);
+            this.lbl3.TabIndex = 10;
+            this.lbl3.Text = "  ";
+            // 
+            // lbl4
+            // 
+            this.lbl4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl4.Location = new System.Drawing.Point(689, 347);
+            this.lbl4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(101, 25);
+            this.lbl4.TabIndex = 11;
+            this.lbl4.Text = "  ";
+            // 
             // frmBarVentas
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.LightSalmon;
-            this.ClientSize = new System.Drawing.Size(961, 596);
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(920, 413);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.lblImporteMozoDia);
+            this.Controls.Add(this.lblTotales);
+            this.Controls.Add(this.lblMozodelDia);
+            this.Controls.Add(this.btnTotales);
+            this.Controls.Add(this.btnMozodelDia);
             this.Controls.Add(this.dgvDatos);
             this.Controls.Add(this.btnValidarDatos);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "frmBarVentas";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Bar La Milanga - Registro";
@@ -145,6 +257,15 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn colBebidasSAlcohol;
         private System.Windows.Forms.DataGridViewTextBoxColumn colBebidasCAlcohol;
         private System.Windows.Forms.DataGridViewTextBoxColumn colPostres;
+        private System.Windows.Forms.Button btnMozodelDia;
+        private System.Windows.Forms.Button btnTotales;
+        private System.Windows.Forms.Label lblMozodelDia;
+        private System.Windows.Forms.Label lblTotales;
+        private System.Windows.Forms.Label lblImporteMozoDia;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl4;
     }
 }
 
--- prySilvaMenendez_SP4_Ej.PorResolver/frmBarRegistroVentas.cs
+++ prySilvaMenendez_SP4_Ej.PorResolver/frmBarRegistroVentas.cs
@@ -17,7 +17,7 @@ namespace prySilvaMenendez_SP4_Ej.PorResolver
             InitializeComponent();
         }
 
-        float[,] matVentas = new float[5,4];
+        float[,] matVentas = new float[100,100];
 
         private void frmBarVentas_Load(object sender, EventArgs e)
         {
@@ -26,41 +26,124 @@ namespace prySilvaMenendez_SP4_Ej.PorResolver
             dgvDatos.Rows.Add("Javier");
             dgvDatos.Rows.Add("Gonzalo");
             dgvDatos.Rows.Add("Alberto");
-            dgvDatos [0,1].ReadOnly = true;
-            dgvDatos [0,2].ReadOnly = true;
-            dgvDatos [0,3].ReadOnly = true;
-            dgvDatos [0,4].ReadOnly = true;
-            dgvDatos [0,0].ReadOnly = true;
             dgvDatos.Columns[0].DefaultCellStyle.BackColor = Color.Maroon;
             dgvDatos.Columns[0].DefaultCellStyle.ForeColor = Color.White;
+            dgvDatos.Columns[0].ReadOnly = true;
             dgvDatos.CurrentCell = dgvDatos.Rows[0].Cells[1];
+            btnMozodelDia.Enabled = false;
+            btnTotales.Enabled = false;
         }
 
         private void btnValidarDatos_Click(object sender, EventArgs e)
         {
-            
+            for (int i = 0; i < dgvDatos.Rows.Count; i++)
+            {
+                for (int j = 1; j < dgvDatos.Columns.Count; j++)
+                {
+                    var valorTexto = dgvDatos.Rows[i].Cells[j].Value?.ToString();
+
+                    if (string.IsNullOrWhiteSpace(valorTexto))
+                        valorTexto = "0";
+
+                    if (!float.TryParse(valorTexto, out float valor))
+                    {
+                        MessageBox.Show(
+                            $"Solo se Permiten Valores Numéricos.\nError en la Celda ({i + 1}, {j}).",
+                            "Error de Validación",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Warning
+                        );
+
+                        dgvDatos.CurrentCell = dgvDatos.Rows[i].Cells[j];
+                        btnMozodelDia.Enabled = false;
+                        btnTotales.Enabled = false;
+                        return;
+                    }
+                    matVentas[i, j - 1] = valor;
+                }
+            }
+            MessageBox.Show("Todos Los Valores son Numéricos.", "Validación Exitosa");
+            btnMozodelDia.Enabled = true;
+            btnTotales.Enabled = true;
         }
 
-        private void dgvDatos_CellValidating(object sender, DataGridViewCellValidatingEventArgs e)
+        private void lblTotales_Click(object sender, EventArgs e)
         {
-            if (e.ColumnIndex == 0)
+
+        }
+
+        private void btnMozodelDia_Click(object sender, EventArgs e)
+        {
+            int filas = matVentas.GetLength(0);
+            int cols = matVentas.GetLength(1); 
+
+            float maxImporte = float.MinValue;
+            int idxMax = -1;
+
+            for (int i = 0; i < filas; i++)
             {
-                return;
-            }   
-            if (string.IsNullOrWhiteSpace(e.FormattedValue.ToString()))
-            {
-                return;
+                float sumaMozo = 0f;
+                for (int j = 0; j < cols; j++)
+                    sumaMozo += matVentas[i, j];
+
+                if (sumaMozo > maxImporte)
+                {
+                    maxImporte = sumaMozo;
+                    idxMax = i;
+                }
             }
 
-            if (!double.TryParse(e.FormattedValue.ToString(), out _))
+            if (idxMax == -1)
             {
-                MessageBox.Show(
-                    $"Solo se Permiten Valores Numéricos.\nError en la Celda ({e.RowIndex + 1}, {e.ColumnIndex}).",
-                    "Error de Validación",
-                    MessageBoxButtons.OK,
-                    MessageBoxIcon.Warning
-                );
-                e.Cancel = true;
+                MessageBox.Show("No hay datos para calcular el mozo del día.", "Atención", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            string nombreMozo = dgvDatos.Rows[idxMax].Cells[0].Value?.ToString() ?? $"Mozo {idxMax + 1}";
+            if (this.Controls.ContainsKey("txtMozoNombre"))
+                lblMozodelDia.Text = nombreMozo;
+            if (this.Controls.ContainsKey("txtMozoImporte"))
+                lblMozodelDia.Text = maxImporte.ToString("F2");
+            if (!this.Controls.ContainsKey("txtMozoNombre") || !this.Controls.ContainsKey("txtMozoImporte"))
+            {
+                MessageBox.Show($"Mozo del día: {nombreMozo}\nImporte: {maxImporte:F2}", "Mozo del día", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnTotales_Click(object sender, EventArgs e)
+        {
+            int filas = matVentas.GetLength(0);
+            int cols = matVentas.GetLength(1);
+
+            float totalGeneral = 0f;
+            float[] totalesPorCategoria = new float[cols];
+
+            for (int j = 0; j < cols; j++)
+                totalesPorCategoria[j] = 0f;
+
+            for (int i = 0; i < filas; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    totalesPorCategoria[j] += matVentas[i, j];
+                    totalGeneral += matVentas[i, j];
+                }
+            }
+
+            if (this.Controls.ContainsKey("txtTotalGeneral"))
+                lblTotales.Text = totalGeneral.ToString("F2");
+
+            if (this.Controls.ContainsKey("txtTotalCat1")) lbl1.Text = totalesPorCategoria[0].ToString("F2");
+            if (this.Controls.ContainsKey("txtTotalCat2")) lbl2.Text = totalesPorCategoria[1].ToString("F2");
+            if (this.Controls.ContainsKey("txtTotalCat3")) lbl3.Text = totalesPorCategoria[2].ToString("F2");
+            if (this.Controls.ContainsKey("txtTotalCat4")) lbl4.Text = totalesPorCategoria[3].ToString("F2");
+            if (!this.Controls.ContainsKey("txtTotalGeneral"))
+            {
+                string msg = $"Total General: {totalGeneral:F2}\n" +
+                             $"Comidas: {totalesPorCategoria[0]:F2}\n" +
+                             $"Bebidas Sin Alcohol: {totalesPorCategoria[1]:F2}\n" +
+                             $"Bebidas Con Alcohol: {totalesPorCategoria[2]:F2}\n" +
+                             $"Postres: {totalesPorCategoria[3]:F2}";
+                MessageBox.Show(msg, "Totales", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
         }
     }
